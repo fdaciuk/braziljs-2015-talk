e68d177445f4359358f804c5286d5fc92a95bd7f
James Halliday <mail@substack.net>
2015-08-21 10:39:22 -0700

    torrent blob store example

---
+++ example/blob/torrent.js
@@ -0,0 +1,13 @@
+var store = require('torrent-blob-store')
+var blob = store({
+  tracker: false,
+  trackers: [ 'udp://localhost:9000' ]
+})
+
+if (process.argv[2] === 'put') {
+  process.stdin.pipe(blob.createWriteStream(function (err, w) {
+    console.log(w.key)
+  }))
+} else if (process.argv[2] === 'get') {
+  blob.createReadStream(process.argv[3]).pipe(process.stdout)
+}
