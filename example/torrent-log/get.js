e364d89594493af21a12838801d7b4550eef42f8
James Halliday <mail@substack.net>
2015-08-21 08:01:03 -0700

    torrent log example works

---
+++ example/torrent-log/get.js
@@ -0,0 +1,13 @@
+var DHT = require('bittorrent-dht')
+var dht = new DHT({ bootstrap: false })
+var hash = Buffer(process.argv[2], 'hex')
+
+dht.listen(5002, function () {
+  dht.addNode('127.0.0.1:5001')
+  dht.once('node', function () {
+    dht.get(hash, function (err, node) {
+      if (err) console.error(err)
+      else console.log(node.v.toString())
+    })
+  })
+})
